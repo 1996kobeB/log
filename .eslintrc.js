4f90115a166374f08fa362f1dc0fc9f68cf8816f
linshuyi <lsy19961996@sina.cn>
2022-03-20 18:52:52 +0800

    update: 输入框完成

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,27 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/essential',
     '@vue/standard'
   ],
   parserOptions: {
     parser: '@babel/eslint-parser'
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'off' : 'off',
+      "vue/multi-word-component-names": ["error", {
+    "ignores": ['index']
+  }],
+  	"vue/html-self-closing": ["error", {
+      "html": {
+        "void": "always",
+        "normal": "never",
+        "component": "always"
+      },
+      "svg": "always",
+      "math": "always"
+    }],
+   'space-before-function-paren':0
   }
 }
---
+++ src/views/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="container">
+    <div class="layui-container">
+      <form class="layui-form layui-form-pane" action="">
+        <div class="layui-form-item">
+          <label class="layui-form-label">用户名</label>
+          <div class="layui-input-block">
+            <input
+              type="text"
+              name="title"
+              required
+              lay-verify="required"
+              placeholder="请输入用户名"
+              autocomplete="off"
+              class="layui-input"
+            />
+          </div>
+        </div>
+        <div class="layui-form-item">
+          <label class="layui-form-label">密码</label>
+          <div class="layui-input-block">
+            <input
+              type="password"
+              name="title"
+              required
+              lay-verify="required"
+              placeholder="请输入密码"
+              autocomplete="off"
+              class="layui-input"
+            />
+          </div>
+        </div>
+        <div class="layui-form-item">
+          <label class="layui-form-label">验证码</label>
+          <div class="layui-input-block">
+            <input
+              type="text"
+              name="title"
+              required
+              lay-verify="required"
+              placeholder="请输入验证码"
+              autocomplete="off"
+              class="layui-input"
+            />
+          </div>
+        </div>
+        <button type="button" class="layui-btn">立即登录</button>
+        <a href="https://www.layuiweb.com" class="link">忘记密码？</a>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  setup() {
+    console.log('lll')
+  }
+}
+</script>
+<style lang="scss" scoped>
+.container {
+  background: #e0e0e0;
+}
+.layui-container {
+  background: #ffffff;
+  input {
+    width: 190px;
+  }
+  .link {
+    margin-left: 10px;
+    &:hover {
+      color: #009688;
+    }
+  }
+}
+</style>
